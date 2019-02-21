837fdaa2318621b5cdbe1c05fa939be00239f21e
visualstudio <visualstudio@230-PC21>
2019-02-21 08:52:28 -0500

    did

--- WEEK7/js/main.js
+++ WEEK7/js/main.js
@@ -1,1 +1,22 @@
-﻿// Your code here!
+﻿// Your code heRE
+var bird = {
+    genus: "corvus",
+    species="corvax",
+    commenname="raven",
+    calltype: "squawky",
+    quote: "nevermore",
+    maxoffspring=5,
+    noisy=true,
+    deadly=false
+};
+
+var bear = {
+    genus: "ursus",
+    species="arctos",
+    commenname="brown bear",
+    calltype: "roar",
+    quote: "",
+    maxoffspring=3,
+    noisy=true,
+    deadly=false
+};
