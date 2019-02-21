e824ab4e5c5d2aabbf5cc6626a7bcffd280d1b50
visualstudio <visualstudio@230-PC21>
2019-02-21 12:00:30 -0500

    did

--- WEEK7/js/main.js
+++ WEEK7/js/main.js
@@ -1,23 +1,25 @@
 ﻿// Your code heRE
 var bird = {
-    genus: "corvus",
-    species="corvax",
-    commenname="raven",
-    calltype: "squawky",
-    quote: "nevermore",
-    maxoffspring=5,
-    noisy=true,
-    deadly=false
+    genus:"corvus",
+    species:"corvax",
+    commenname:"raven",
+    calltype:"squawky",
+    quote:"nevermore",    
+    maxoffspring:5,
+    noisy:true,
+    deadly:false,
 };
-
 var bear = {
-    genus: "ursus",
-    species="arctos",
-    commenname="brown bear",
-    calltype: "roar",
-    quote: "",
-    maxoffspring=3,
-    noisy=true,
-    deadly=false
-
+    genus:"ursus",
+    species:"arctos",
+    commenname:"brown bear",
+    calltype:"roar",
+    quote:"",
+    maxoffspring:3,
+    noisy:true,
+    deadly:true,
 };
+
+var cities = [
+    'toronto', 'mississsuaga', 'brampton', 'etobicoke', 'malton'
+];
